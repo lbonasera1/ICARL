cd63607b774a6e1b3cb8f4d00e9a6db1f8e51fe4
lbonasera1 <64270247+lbonasera1@users.noreply.github.com>
2020-05-30 13:03:05 +0200

    Update resnet_cifar.py

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -44,46 +44,6 @@ class BasicBlock(nn.Module):
 
         return out
 
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=100):
@@ -138,17 +98,7 @@ class ResNet(nn.Module):
 
         return x
 
-def resnet20(pretrained=False, **kwargs):
-    n = 3
-    model = ResNet(BasicBlock, [n, n, n], **kwargs)
-    return model
-
 def resnet32(pretrained=False, **kwargs):
     n = 5
     model = ResNet(BasicBlock, [n, n, n], **kwargs)
-    return model
-
-def resnet56(pretrained=False, **kwargs):
-    n = 9
-    model = ResNet(Bottleneck, [n, n, n], **kwargs)
     return model

0b571edeecfebfa590a6e9e57afcfaa5f0a3041e
lbonasera1 <64270247+lbonasera1@users.noreply.github.com>
2020-06-02 12:48:14 +0200

    Update resnet_cifar.py

--- resnet_cifar.py
+++ resnet_cifar.py
@@ -49,8 +49,7 @@ class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=100):
         self.inplanes = 16
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,
-                               bias=False)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)   # icarl uses bias!
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, 16, layers[0])

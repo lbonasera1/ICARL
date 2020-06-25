9f921671f465ea26b244c31075ffb7673cbebd54
lbonasera1 <64270247+lbonasera1@users.noreply.github.com>
2020-06-25 16:34:43 +0200

    Update cosine_linear_layer.py

--- cosine_linear_layer.py
+++ cosine_linear_layer.py
@@ -6,26 +6,21 @@ from torch.nn import functional as F
 from torch.nn import Module
 
 class CosineLinear(Module):
-    def __init__(self, in_features, out_features, sigma=True):
+    def __init__(self, in_features, out_features):
         super(CosineLinear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.weight = Parameter(torch.Tensor(out_features, in_features))
-        if sigma:
-            self.sigma = Parameter(torch.Tensor(1))
-        else:
-            self.register_parameter('sigma', None)
+        self.sigma = Parameter(torch.Tensor(1))
         self.reset_parameters()
 
     def reset_parameters(self):
         stdv = 1. / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
-        if self.sigma is not None:
-            self.sigma.data.fill_(1) #for initializaiton of sigma
+        self.sigma.data.fill_(1)
 
     def forward(self, input):
         out = F.linear(F.normalize(input, p=2,dim=1), \
                 F.normalize(self.weight, p=2, dim=1))
-        if self.sigma is not None:
-            out = self.sigma * out
+        out = self.sigma * out
         return out
